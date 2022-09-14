afd99bab635a13af880b842ca34a64d5d287a368
Videl Smith <gitlab@videlsmith.com>
2022-09-14 13:20:05 -0500

    updated sensor code

--- soil_sensor.py
+++ soil_sensor.py
@@ -4,17 +4,35 @@ import utime
 '''
 Code Notes
 
-Add External LED as an option
-Add conversion factor(3.3/65535)
 Add light vs deep sleep vs pushToCheck options
 Add C++ Notes
 '''
 
 
 # initialize vars
+led_onboard = Pin(25, Pin.OUT)
+led_external = Pin(15, Pin.OUT, Pin.PULL_DOWN)
 moisture_sensor = ADC(28)
+conversion_factor = 3.3/65535
 
 # primary application loop
 while True:
-    print(moisture_sensor.read_u16())
+    # onboard lights up when we read
+    led_onboard.value(1)
+
+    # check the sensor
+    sensor_reading = moisture_sensor.read_u16()
+    converted_reading = sensor_reading * conversion_factor
+    print(converted_reading)
+
+    # if below threshold, turn on water indicator
+    if converted_reading > 1.4:
+        led_external.value (1)
+    else:
+        led_external.value(0)
+
+    # wait one second and then reset the led
     utime.sleep(1)
+    led_onboard.value(0)
+    utime.sleep(1) 
+
